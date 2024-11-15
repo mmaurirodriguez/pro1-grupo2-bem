9ef45f8cac62f6e325c74bf7f87803d7e4aefcb1
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2024-11-15 12:10:21 -0300

    Cree htmls, usé java para conseguir los detalles de las recetas (dudas con el fetch)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-console.log(hola);
+fetch('https://dummyjson.com/recipes/meal-type/snack')
+.then(res => res.json())
+.then(console.log);
+
+const URL =  "https://dummyjson.com/recipes/meal-type/snack";
+let listado = document.querySelector(".recetas");
+
+fetch(URL)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    const recetas = data.recipes;
+
+
+
+})
+.catch(function(err) {
+    return console.log(err);
+    ;
+});
