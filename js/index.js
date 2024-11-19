46371d9a71a740ab999432dd905ec117a6cf5c07
BrisaPereyra23 <bpereyra@udesa.edu.ar>
2024-11-19 15:00:50 -0300

    js de index, verificar formulario

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const URL =  "https://dummyjson.com/recipes?limit=10&skip=10&select=name,image";
+/*const URL =  "https://dummyjson.com/recipes?limit=10&skip=10&select=name,image";
 let listado = document.querySelector(".recetas");
 
 fetch(URL)
@@ -12,4 +12,20 @@ fetch(URL)
 .catch(function(err) {
     return console.log(err);
     ;
-});
+});*/
+let formulario = document.querySelector("form");
+let campoBuscar = document.querySelector(".buscar");
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (campoBuscar.value == ""){
+        alert("Completá el campo. No puede estar vacío")
+    }
+    else if(campoBuscar.value.length < 4) {
+            alert("Debes ingresar una palabra mayor a 3 caracteres")
+    }
+    else{
+        formulario.submit()
+    }
+})
+/*arreglar, no me funciona*/
