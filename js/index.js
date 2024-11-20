fc39b0ab01fb4cf988d0f5438ffc9a7bcbaf37ce
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2024-11-19 22:01:53 -0300

    Correccion del JS del index y creacion de la base para traer la info de las apis (primeros 10)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-/*const URL =  "https://dummyjson.com/recipes?limit=10&skip=10&select=name,image";
+let buscador = document.querySelector(".buscador");
+let formulario = document.querySelector("form");
+
+//APIS E INFO//
+const URL =  "https://dummyjson.com/recipes?limit=10&skip=0&select=name,image";
 let listado = document.querySelector(".recetas");
 
 fetch(URL)
@@ -12,20 +16,19 @@ fetch(URL)
 .catch(function(err) {
     return console.log(err);
     ;
-});*/
-let formulario = document.querySelector("form");
-let campoBuscar = document.querySelector(".buscar");
+});
 
-formulario.addEventListener("submit", function (event) {
-    event.preventDefault();
-    if (campoBuscar.value == ""){
-        alert("Completá el campo. No puede estar vacío")
+//BUSCADOR FORMULARIO//
+formulario.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let valorNombre = buscador.value;
+
+    if (!valorNombre) {
+        alert("Debes completar el campo nombre");
+    } else if (valorNombre.length < 3) {
+        alert("El nombre debe tener al menos 3 caracteres");
+    } else {
+        formulario.submit();
     }
-    else if(campoBuscar.value.length < 4) {
-            alert("Debes ingresar una palabra mayor a 3 caracteres")
-    }
-    else{
-        formulario.submit()
-    }
-})
-/*arreglar, no me funciona*/
+});
+
