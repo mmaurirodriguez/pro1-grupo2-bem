9981b98ed499c44c7bbf5124b24d17af373c85e4
BrisaPereyra23 <bpereyra@udesa.edu.ar>
2024-11-20 17:27:50 -0300

    hice el js de login y acomode la estructura del html

--- js/login.js
+++ js/login.js
@@ -1,1 +1,19 @@
-/*alert para email y contraseña*/
+// login.js
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+    if (email === "") {
+        alert("Por favor complete el campo email");
+        return;
+    }
+
+    if (password === "") {
+        alert("Por favor complete el campo contraseña");
+        return;
+    }
+
+});
+
