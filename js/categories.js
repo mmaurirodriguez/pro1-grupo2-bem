320d5fe5687758f38416dbd4508d2ae7c3b50420
BrisaPereyra23 <bpereyra@udesa.edu.ar>
2024-11-21 12:05:21 -0300

    Agregue el catch que me habia olvidado

--- js/categories.js
+++ js/categories.js
@@ -8,20 +8,23 @@ let skip = 0;
 
 //APIS E INFO//
 const URL = "https://dummyjson.com/recipes/tags?limit=10&skip=" + skip;
-function cargarMasrecetas(url) {
-    fetch(url)
-    .then(function(data) {
-        return data.json();
-    })
-    .then(function(results) {
-        console.log(results);
-        let dato = results;
-        let contenido = "";
-        let tagsParaMostrar = dato.slice(skip, skip + 10);
-
-        for (let i = 0; i < tagsParaMostrar.length; i++) {
-            contenido += `<li><a href="./category.html">${tagsParaMostrar[i]}</a></li>`;
-        }
-        listaRecetas.innerHTML += contenido;
-    }); 
-}
+    function cargarMasrecetas(url) {
+        fetch(url)
+            .then(function(data) {
+                return data.json();
+            })
+            .then(function(results) {
+                console.log(results);
+                let dato = results;
+                let contenido = "";
+                let tagsParaMostrar = dato.slice(skip, skip + 10);
+    
+                for (let i = 0; i < tagsParaMostrar.length; i++) {
+                    contenido += `<li><a href="./category.html">${tagsParaMostrar[i]}</a></li>`;
+                }
+                listaRecetas.innerHTML += contenido;
+            })
+            .catch(function(err) {
+                console.log(err);
+            }); 
+    }
