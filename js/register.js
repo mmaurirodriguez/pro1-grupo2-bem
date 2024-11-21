07843a7bd0fb11a8ca797701dcdee277a17ada40
Emma <emagnin@udesa.edu.ar>
2024-11-21 14:47:53 -0300

    hice modificaciones en el css y corregi register.js

--- js/register.js
+++ js/register.js
@@ -1,25 +1,28 @@
-document.getElementById('FormReg').addEventListener ('submit', function(event)) {
-{
+document.getElementById('FormReg').addEventListener('submit', function (event) {
     event.preventDefault(); 
+
     const password = document.getElementById('password').value;
     const email = document.getElementById('email').value;
-    const paswordError = document.getElementById('passwordError');
+    const passwordError = document.getElementById('passwordError');
     const emailError = document.getElementById('emailError');
+
     emailError.textContent = '';
     passwordError.textContent = '';
-    let valid = true; 
-} 
-document.getElementById('FormReg').addEventListener('submit', function(event)){
-    if (!email){
-        emailError.textContent = 'Complete el campo porfavor';
-        valid = false
-    } 
+
+    let valid = true; // Bandera para validar
+
+    // Validar email
+    if (!email) {
+        emailError.textContent = 'Complete el campo por favor';
+        valid = false;
+    }
+
+    // Validar contraseña
     if (!password) {
-        passwordError.textContent = 'Complete el campo porfavor';
-        valid = false
+        passwordError.textContent = 'Complete el campo por favor';
+        valid = false;
     }
     if (valid) {
-        window.location.href = 'login.html'
+        window.location.href = 'login.html';
     }
-}; 
-}
+});
